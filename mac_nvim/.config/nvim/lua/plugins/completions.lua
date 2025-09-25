8770231ffcd5da520f84ddb1d8e5d7afe4b1749f
Yuri Nana Hummel <yurihummel.dev@gmail.com>
2025-09-25 15:24:44 -0300

    snippets

--- mac_nvim/.config/nvim/lua/plugins/completions.lua
+++ mac_nvim/.config/nvim/lua/plugins/completions.lua
@@ -6,12 +6,14 @@ return {
 		"L3MON4D3/LuaSnip",
 		dependencies = {
 			"saadparwaiz1/cmp_luasnip",
+			"rafamadriz/friendly-snippets",
 		},
 	},
 	{
 		"hrsh7th/nvim-cmp",
 		config = function()
 			local cmp = require("cmp")
+			require("luasnip.loaders.from_vscode").lazy_load()
 
 			cmp.setup({
 				snippet = {
