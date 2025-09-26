5cef84958f4c57cac6cc9567a0a52019637bdc4d
Yuri Nana Hummel <yurihummel.dev@gmail.com>
2025-09-25 22:27:12 -0300

    java config

--- mac_nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ mac_nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -1,31 +1,46 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end,
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "gopls" },
-            })
-        end,
-    },
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            local capabilities = require("cmp_nvim_lsp").default_capabilities()
-            vim.lsp.config('*',{ capabilities = capabilities })
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "gopls" },
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			vim.lsp.config("*", { capabilities = capabilities })
 
-            vim.keymap.set("n", "<space>fd", vim.diagnostic.open_float)
-            vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
-            vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-            vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-            vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, {})
-            vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, {})
-        end,
-    },
+            -- Java config
+			vim.uv.os_setenv("JAVA_HOME", "/Users/yurihummel/.sdkman/candidates/java/21.0.8-tem")
+			vim.lsp.config("jdtls", {
+				runtimes = {
+					{
+						name = "JavaSE-11",
+						path = "/Users/yurihummel/.sdkman/candidates/java/11.0.28-tem",
+					},
+					{
+						name = "JavaSE-21",
+						path = "/Users/yurihummel/.sdkman/candidates/java/21.0.8-tem",
+					},
+				},
+			})
+
+			vim.keymap.set("n", "<space>fd", vim.diagnostic.open_float)
+			vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+			vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, {})
+			vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, {})
+		end,
+	},
 }
