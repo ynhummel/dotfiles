dae8036d46ba06c7b3049340677d8d9e2b8176fc
Yuri Nana Hummel <yurihummel.dev@gmail.com>
2025-10-01 08:38:27 -0300

    improve snippets

--- mac_nvim/.config/nvim/lua/plugins/completions.lua
+++ mac_nvim/.config/nvim/lua/plugins/completions.lua
@@ -39,6 +39,19 @@ return {
 					{ name = "buffer" },
 				}),
 			})
+
+			local ls = require("luasnip")
+			vim.keymap.set({ "i", "s" }, "<c-k>", function()
+				if ls.expand_or_jumpable() then
+					ls.expand_or_jump()
+				end
+			end, { silent = true })
+
+			vim.keymap.set({ "i", "s" }, "<c-j>", function()
+				if ls.jumpable(-1) then
+					ls.jump(-1)
+				end
+			end, { silent = true })
 		end,
 	},
 }
